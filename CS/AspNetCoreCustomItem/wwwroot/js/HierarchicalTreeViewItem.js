e088f9cdb3833278f99e0d3d8991be46d2f3cba8
Yaroslav <40101877+Yaroslav-Kuznetsov@users.noreply.github.com>
2022-11-08 20:23:36 +0400

    Update HierarchicalTreeViewItem.js
    Making the item name unique

--- CS/AspNetCoreCustomItem/wwwroot/js/HierarchicalTreeViewItem.js
+++ CS/AspNetCoreCustomItem/wwwroot/js/HierarchicalTreeViewItem.js
@@ -2,7 +2,7 @@
     const Dashboard = DevExpress.Dashboard;
     const dxTreeView = DevExpress.ui.dxTreeView;
 
-    const HIERARCIAL_TREE_VIEW_EXTENSION_NAME = 'TreeView';
+    const HIERARCIAL_TREE_VIEW_EXTENSION_NAME = 'CustomTreeView';
 
     const svgIcon = `<?xml version="1.0" encoding="utf-8"?>
         <svg version="1.1" id="` + HIERARCIAL_TREE_VIEW_EXTENSION_NAME +`" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 24 24" style="enable-background:new 0 0 24 24;" xml:space="preserve">
