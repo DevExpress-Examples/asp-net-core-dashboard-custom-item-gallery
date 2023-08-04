6284a02d5115f0e58987605b9208cc7867252b61
Andrey Lepikhov (auto) <andrey.lepikhov@devexpress.com>
2023-08-04 16:15:47 +0400

    auto sync for version 2023.1

--- CS/AspNetCoreCustomItem/Program.cs
+++ CS/AspNetCoreCustomItem/Program.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCoreCustomItemGallery.Models;
+using DevExpress.Utils;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -13,6 +15,7 @@ namespace AspNetCoreCustomItemGallery
     {
         public static void Main(string[] args)
         {
+            DeserializationSettings.RegisterTrustedClass(typeof(TasksData));
             CreateHostBuilder(args).Build().Run();
         }
 
